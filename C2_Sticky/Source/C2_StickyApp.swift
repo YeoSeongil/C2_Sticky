d2d300e6a32e9d6972376df10b5a84322f441178
Yeo Seongil <seongil_yeo@naver.com>
2025-04-14 20:46:46 +0900

    [init] 프로젝트 생성

---
+++ C2_Sticky/Source/C2_StickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2_StickyApp.swift
+//  C2_Sticky
+//
+//  Created by 성일 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct C2_StickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
